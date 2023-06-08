24d36bf888ee517f180e454dbfef87e3243ed6c5
jep4 <civbf1@gmail.com>
2023-06-08 05:31:35 -0700

    board finish

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const gall_icon = document.getElementById('gall_img');
 gall_icon.addEventListener('click', () => {
     if (board.style.display == 'block') {
         board.style.display = 'none';
+        embed.style.display = 'none';
         gall.style.display = 'block';
 
     }
@@ -23,19 +24,24 @@ gall_icon.addEventListener('click', () => {
 
 const board = document.getElementById('board');
 const diary_img = document.getElementById('diary_img');
+const embed = document.getElementById('embed1');
+
 
 diary_img.addEventListener('click', () => {
     if (gall.style.display == 'block') {
         gall.style.display = 'none';
         board.style.display = 'block';
+        embed.style.display = 'block';
 
     }
     else {
         if (board.style.display == 'block') {
             board.style.display = 'none';
+            embed.style.display = 'none';
         }
         else {
             board.style.display = 'block';
+            embed.style.display = 'block';
 
         }
 
