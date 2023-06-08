efe21e06add839d7270926535ba9f00f375552ae
jep4 <civbf1@gmail.com>
2023-06-08 04:17:40 -0700

    organize position

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// JavaScript source code
+
+$(document).ready(function () {
+    $("body").addClass("in");
+});
+
+$(".transition").on("click", function (event) {
+    event.preventDefault();
+    let href = $(this).attr("href");
+
+    $("body").removeClass("in").addClass("out");
+    setTimeout(() => {
+        window.location.href = href;
+    }, 500);
+});
