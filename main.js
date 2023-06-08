26961f2347c097b1452ee982a9bea1e9748ec0ac
jep4 <civbf1@gmail.com>
2023-06-08 05:17:22 -0700

    gallery and diary frame

---
+++ main.js
@@ -0,0 +1,43 @@
+
+const gall = document.getElementById('gall');
+const gall_icon = document.getElementById('gall_img');
+
+gall_icon.addEventListener('click', () => {
+    if (board.style.display == 'block') {
+        board.style.display = 'none';
+        gall.style.display = 'block';
+
+    }
+    else {
+        if (gall.style.display == 'block') {
+            gall.style.display = 'none';
+        }
+        else {
+            gall.style.display = 'block';
+
+        }
+
+    }
+});
+
+
+const board = document.getElementById('board');
+const diary_img = document.getElementById('diary_img');
+
+diary_img.addEventListener('click', () => {
+    if (gall.style.display == 'block') {
+        gall.style.display = 'none';
+        board.style.display = 'block';
+
+    }
+    else {
+        if (board.style.display == 'block') {
+            board.style.display = 'none';
+        }
+        else {
+            board.style.display = 'block';
+
+        }
+
+    }
+});
