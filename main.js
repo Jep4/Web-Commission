a1b4d0c505aaed3b9ec3950e995dd18c3a4ea1f4
jep4 <civbf1@gmail.com>
2023-06-12 03:19:23 -0700

    final

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 const gall = document.getElementById('gall_menu');
 const gall_icon = document.getElementById('gall_img');
 const listBox = document.getElementById('list_box');
+const board = document.getElementById('board');
+const diary_img = document.getElementById('diary_img');
+const embed = document.getElementById('embed1');
+var images = document.querySelectorAll("#img_list>li>a");
+var frame = document.getElementById("frame");
+var imgBox = document.getElementById('img_box');
+var imgs = document.querySelectorAll("#img_box2>li");
+
 
 gall_icon.addEventListener('click', () => {
     if (board.style.display == 'block') {
@@ -9,6 +17,7 @@ gall_icon.addEventListener('click', () => {
         embed.style.display = 'none';
         gall.style.display = 'block';
         listBox.style.display = 'block';
+        imgBox.style.display = 'block';
 
     }
     else {
@@ -16,10 +25,10 @@ gall_icon.addEventListener('click', () => {
             gall.style.display = 'none';
             frame.style.display = 'none';
             listBox.style.display = 'none';
+            imgBox.style.display = 'none';
         }
         else {
             gall.style.display = 'block';
-            frame.style.display = 'none';
             listBox.style.display = 'block';
 
         }
@@ -27,16 +36,11 @@ gall_icon.addEventListener('click', () => {
     }
 });
 
-
-const board = document.getElementById('board');
-const diary_img = document.getElementById('diary_img');
-const embed = document.getElementById('embed1');
-
-
 diary_img.addEventListener('click', () => {
     if (gall.style.display == 'block') {
         gall.style.display = 'none';
         frame.style.display = 'none';
+        imgBox.style.display = 'none';
         listBox.style.display = 'none';
         board.style.display = 'block';
         embed.style.display = 'block';
@@ -56,12 +60,17 @@ diary_img.addEventListener('click', () => {
     }
 });
 
-var images = document.querySelectorAll("#img_list>li>a");
-var frame = document.getElementById("frame");
-
-for (var i = 0; i < images.length; i++) {
+for (let i = 0; i < images.length; i++) {
     images[i].addEventListener("click", function (e) {
         e.preventDefault();
+
+        for (let k = 0; k < imgs.length; k++) {
+            imgs[k].style.display = 'none';
+        }
         frame.style.display = 'block';
+        imgBox.style.display = 'block';
+        imgs[i].style.display = 'block';
     });
 }
+
+
