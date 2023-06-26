0cf71205a8c2d7c396ba035179a001139b89e94f
jep4 <civbf1@gmail.com>
2023-06-25 20:00:31 -0700

    all menus

--- main.js
+++ main.js
@@ -1,70 +1,145 @@
 
-const gall = document.getElementById('gall_menu');
-const gall_icon = document.getElementById('gall_img');
-const listBox = document.getElementById('list_box');
+//Windows
 const board = document.getElementById('board');
-const diary_img = document.getElementById('diary_img');
-const embed = document.getElementById('embed1');
+const embed = document.getElementsByClassName('embed');
+
+//Icons 
+const about_icon = document.getElementById('about_img');
+const book_icon = document.getElementById('book_img');
+const diary_icon = document.getElementById('diary_img');
+const txt_icon = document.getElementById('text_img');
+const rep_icon = document.getElementById('rep_img');
+const mes_icon = document.getElementById('mes_img');
+const h1_icon = document.getElementById('h1_img');
+const h2_icon = document.getElementById('h2_img');
+const gall_icon = document.getElementById('gall_img');
+
+//Gallery
+
+const gall = document.getElementById('gall_menu');
 var images = document.querySelectorAll("#img_list>li>a");
 var frame = document.getElementById("frame");
 var imgBox = document.getElementById('img_box');
 var imgs = document.querySelectorAll("#img_box2>li");
+const listBox = document.getElementById('list_box');
 
 
-gall_icon.addEventListener('click', () => {
+function winMain(bNum) {
+    //About
+    if (bNum == 0) {
+        //If --> Target is on
+        if (board.style.display == 'block') {
+            closeAll();
+        }
+
+        //If --> Empty
+        //If --> Other page is on
+        else {
+            closeAll();
+            board.style.display = 'block';
+        }
+
+    }
+    else {
+        bNum--;
+        el = embed[bNum];
+        //If --> Target is on
+        if (el.style.display == 'block') {
+            closeAll();
+        }
+
+        //If --> Empty
+        //If --> Other page is on
+        else {
+            closeAll();
+            board.style.display = 'block';
+            el.style.display = 'block';
+        }
+    }
+
+
+
+
+}
+
+function closeAll() {
+    for (let i = 0; i < embed.length; i++) {
+        embed[i].style.display = 'none';
+    }
 
     for (let k = 0; k < imgs.length; k++) {
         imgs[k].style.display = 'none';
     }
-    if (board.style.display == 'block') {
-        board.style.display = 'none';
-        embed.style.display = 'none';
+
+    for (let k = 0; k < images.length; k++) {
+        images[k].style.display = 'none';
+    }
+
+    board.style.display = 'none';
+    gall.style.display = 'none';
+    frame.style.display = 'none';
+    imgBox.style.display = 'none';
+    listBox.style.display = 'none';
+}
+
+
+//About Icon
+about_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(0);
+});
+//Book Icon
+book_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(1);
+});
+//Diary Icon
+diary_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(2);
+});
+//Text Icon
+txt_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(3);
+});
+//Report Icon
+rep_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(4);
+});
+//Message Icon
+mes_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(5);
+});
+//Heart 1 Icon
+h1_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(6);
+});
+//Heart 2 Icon
+h2_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    winMain(7);
+});
+//Gallery 2 Icon
+gall_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    if (gall.style.display == 'block') {
+        closeAll();
+    }
+    else {
+        closeAll();
         gall.style.display = 'block';
         listBox.style.display = 'block';
-
-    }
-    else {
-        if (gall.style.display == 'block') {
-            gall.style.display = 'none';
-            frame.style.display = 'none';
-            listBox.style.display = 'none';
-            imgBox.style.display = 'none';
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.display = 'block';
         }
-        else {
-            gall.style.display = 'block';
-            listBox.style.display = 'block';
-
-        }
-
     }
 });
 
-diary_img.addEventListener('click', () => {
-    for (let k = 0; k < imgs.length; k++) {
-        imgs[k].style.display = 'none';
-    }
-    if (gall.style.display == 'block') {
-        gall.style.display = 'none';
-        frame.style.display = 'none';
-        imgBox.style.display = 'none';
-        listBox.style.display = 'none';
-        board.style.display = 'block';
-        embed.style.display = 'block';
 
-    }
-    else {
-        if (board.style.display == 'block') {
-            board.style.display = 'none';
-            embed.style.display = 'none';
-        }
-        else {
-            board.style.display = 'block';
-            embed.style.display = 'block';
-
-        }
-
-    }
-});
 
 for (let i = 0; i < images.length; i++) {
     images[i].addEventListener("click", function (e) {
