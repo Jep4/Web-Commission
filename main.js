e2de4c4bc47ccafd9bd8d396905fc25b7b90553e
jep4 <civbf1@gmail.com>
2023-06-25 20:58:52 -0700

    fixed about button bug

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 //Windows
 const board = document.getElementById('board');
 const embed = document.getElementsByClassName('embed');
+const count = document.getElementById('count_img');
 
 //Icons 
+const logo_icon = document.getElementById('logo_img');
 const about_icon = document.getElementById('about_img');
 const book_icon = document.getElementById('book_img');
 const diary_icon = document.getElementById('diary_img');
@@ -24,16 +26,30 @@ var imgs = document.querySelectorAll("#img_box2>li");
 const listBox = document.getElementById('list_box');
 
 
-function winMain(bNum) {
+function winMain(bNum, isAbt) {
     //About
     if (bNum == 0) {
-        //If --> Target is on
-        if (board.style.display == 'block') {
+        //If --> Other page is on
+        if (gall.style.display != 'none') {
             closeAll();
+            board.style.display = 'block';
+        }
+        //If --> Other page is on
+
+        else if (board.style.display != 'none') {
+
+            //If --> Target is on
+            if (isAbt == true) {
+                board.style.display = 'none';
+                count.style.display = 'block';
+            }
+            else {
+                closeAll();
+                board.style.display = 'block';
+            }
         }
 
         //If --> Empty
-        //If --> Other page is on
         else {
             closeAll();
             board.style.display = 'block';
@@ -46,6 +62,7 @@ function winMain(bNum) {
         //If --> Target is on
         if (el.style.display == 'block') {
             closeAll();
+            count.style.display = 'block';
         }
 
         //If --> Empty
@@ -80,48 +97,63 @@ function closeAll() {
     frame.style.display = 'none';
     imgBox.style.display = 'none';
     listBox.style.display = 'none';
+    count.style.display = 'none';
 }
 
+var isA = false;
+//Logo Icon
+logo_icon.addEventListener('click', () => {
+    console.log("Button clicked");
+    closeAll();
+});
 
 //About Icon
 about_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(0);
+    winMain(0, isA);
+    isA = true;
 });
 //Book Icon
 book_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(1);
+    winMain(1, isA);
+    isA = false;
 });
 //Diary Icon
 diary_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(2);
+    winMain(2, isA);
+    isA = false;
 });
 //Text Icon
 txt_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(3);
+    winMain(3, isA);
+    isA = false;
 });
 //Report Icon
 rep_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(4);
+    winMain(4, isA);
+    isA = false;
 });
 //Message Icon
 mes_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(5);
+    winMain(5, isA);
+    isA = false;
 });
 //Heart 1 Icon
 h1_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(6);
+    winMain(6, isA);
+    isA = false;
 });
 //Heart 2 Icon
 h2_icon.addEventListener('click', () => {
     console.log("Button clicked");
-    winMain(7);
+    winMain(7, isA);
+    isA = false;
 });
 //Gallery 2 Icon
 gall_icon.addEventListener('click', () => {
